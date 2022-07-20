85ff9f74d604bd8b31007f67437cbd31f53a0f1e
José Antônio Oliveira de Freitas <jfreitas@unb.br>
2022-07-20 20:32:49 -0300

    Textos em português e inglês.

--- metodo_gaus_jordan_3x3.py
+++ metodo_gaus_jordan_3x3.py
@@ -27,7 +27,7 @@ for eqn in equations_array:
 augmented_matrix_colors = [RED, GREEN, BLUE, TEAL]
 
 ####Textos presentes no vídeo############
-texto_br = True
+texto_br = False
 
 if texto_br == True:
     title = Tex(r"Encontre a solução do sistema linear:")
@@ -76,51 +76,51 @@ if texto_br == True:
 
     passo_13b = Tex(r"está na forma linha reduzida à forma em escada. Com isso\\ o sistema admite uma única solução que será:")
 else:
-    title = Tex(r"Encontre a solução do sistema linear:")
+    title = Tex(r"Find the solution of the linear system:")
 
-    subtitle = Tex(r"Vamos usar o Método de eliminação de Gauss-Jordam para encontrar a solução do sistema.")
+    subtitle = Tex(r"We are going to apply the Gauss-Jordan Algorithm to find the solution of the linear system.")
 
-    primeiro_passo = Tex(r"Primeiro montamos a matriz amplidada do sistema:")
+    primeiro_passo = Tex(r"First we write the augmented matrix of the system:")
 
-    passo_1 = Tex(r"Como esse coeficiente é diferente de 1 \\ vamos trocar a primeira linha com a segunda linha.")
+    passo_1 = Tex(r"This coefficient is different from 1 \\ so we are going to swap the first and second row.")
 
-    passo_2= Tex(r"Agora vamos zerar os coeficiente \\ na primeira coluna que estão abaixo do 1.")
+    passo_2= Tex(r"Now lets cancel the coefficients in the first column thar are bellow 1.")
 
-    passo_3a = Tex(r"Começamos multiplicando a primeira linha por -3.")
+    passo_3a = Tex(r"We start by multiplying the first row by -3.")
 
-    passo_3b = Tex(r"E somamos com a segunda linha.")
+    passo_3b = Tex(r"And we add it to the second row.")
 
-    passo_4a = Tex(r"Agora multiplicamos a primeira linha por -2.")
+    passo_4a = Tex(r"Now we multiply the first row by -2.")
 
-    passo_4b = Tex(r"E somamos com a terceira linha.")
+    passo_4b = Tex(r"And we add it to the third row.")
 
-    passo_5 = Tex(r"Como esse coeficiente é diferente de 1 \\ vamos multiplicar a segunda linha por 1/10.")
+    passo_5 = Tex(r"This coefficient is different from 1, so we are going to multiply the second row by 1/10.")
 
-    passo_6= Tex(r"Agora vamos zerar os coeficiente \\ na segunda coluna que estão abaixo e acima do 1.")
+    passo_6= Tex(r"Now lets cancel the coefficients in the second row that are above 1.")
 
-    passo_7a = Tex(r"Começamos multiplicando a segunda linha por 3.")
+    passo_7a = Tex(r"We start by multiplying the second row by 3.")
 
-    passo_7b = Tex(r"E somamos com a primeira linha.")
+    passo_7b = Tex(r"And we add it to the first row.")
 
-    passo_8a = Tex(r"Agora vamos multiplicar a segunda linha por -4.")
+    passo_8a = Tex(r"Now multiply the second row by -4.")
 
-    passo_8b = Tex(r"E somamos com a terceira linha.")
+    passo_8b = Tex(r"And add it to the third row.")
 
-    passo_9 = Tex(r"Como esse coeficiente é diferente de 1 \\ vamos multiplicar a terceira linha por -5/7.")
+    passo_9 = Tex(r"This coefficient is different from 1 and so we multiply the third row by -5/7.")
 
-    passo_10 = Tex(r"Agora vamos zerar os coeficiente \\ na terceira coluna que estão acima do 1.")
+    passo_10 = Tex(r"Lets cancel the coefficients in the third column that are above 1.")
 
-    passo_11a = Tex(r"Começamos multiplicando a terceira linha por 7/5.")
+    passo_11a = Tex(r"Multiply the third row by 7/5.")
 
-    passo_11b = Tex(r"E somamos com a segunda linha.")
+    passo_11b = Tex(r"And add it to the second row.")
 
-    passo_12a = Tex(r"Agora multiplicamos a terceira linha por 1/5.")
+    passo_12a = Tex(r"Then multiply the third row by 1/5.")
 
-    passo_12b = Tex(r"E somamos com a primeira linha.")
+    passo_12b = Tex(r"And add it to the first row.")
 
-    passo_13a = Tex(r"A matriz resultante desse último passo:")
+    passo_13a = Tex(r"The matrix resulting from this last step:")
 
-    passo_13b = Tex(r"está na forma linha reduzida à forma em escada. Com isso\\ o sistema admite uma única solução que será:")
+    passo_13b = Tex(r"is in the reduced row echolon form. Thus, the linear systema admits a unique solution that will be:")
 
 
 def finishScene(self):
@@ -710,3 +710,5 @@ class Start(Scene):
 
             self.remove(row)
         finishScene(self)
+
+        l.fechamento(self)
