8556ab598aa8b0f84a50613e253445bd354846b5
José Antônio Oliveira de Freitas <jfreitas@unb.br>
2022-07-20 14:23:56 -0300

    Removendo código que está na classe Logo agora.

--- metodo_gaus_jordan_3x3.py
+++ metodo_gaus_jordan_3x3.py
@@ -26,38 +26,6 @@ for eqn in equations_array:
 
 augmented_matrix_colors = [RED, GREEN, BLUE, TEAL]
 
-
-text = VGroup()
-text1 = Tex(r"$\Sigma$", color=RED,font_size=174)
-text2 = Text("Math", font="Tex Gyre Chorus", slant=ITALIC, font_size = 58)
-text3 = Text("Exercicises", font="Tex Gyre Chorus", slant=ITALIC, font_size = 58)
-
-text.add(text2, text3)
-text.arrange(DOWN, center = False, aligned_edge = LEFT)
-
-text.next_to(text1, RIGHT)
-
-texto_logo = VGroup()
-
-texto_logo.add(text1, text)
-circle_logo = Circle(fill_opacity=5).scale(2.2)
-circle_logo.set_fill()
-circle_logo.set_color(GREEN)
-
-circle_logo_right_corner = circle_logo.scale(0.2)
-texto_logo_right_corner = texto_logo.scale(0.2)
-
-def logo_principal():
-    texto_logo.next_to(circle_logo, 0, buff=1).scale(0.9)
-
-    return texto_logo
-
-def logo_right_corner(texto_logo_right_corner, circle_logo_right_corner):
-    return VGroup(circle_logo_right_corner.to_corner(DOWN + RIGHT), texto_logo_right_corner.next_to(circle_logo_right_corner, 0, buff = 1))
-
-def finishScene(self):
-    self.play(*[FadeOut(mob) for mob in self.mobjects])
-
 class Start(Scene):
     def construct(self):
         texto_logo = logo_principal()
