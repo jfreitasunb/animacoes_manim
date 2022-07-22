3b45dd2354535c62b14def664f237950a1157ecf
José Antônio Oliveira de Freitas <jfreitas@unb.br>
2022-07-22 15:42:43 -0300

    Melhorando a leitura do código.

--- logo.py
+++ logo.py
@@ -1,10 +1,15 @@
 from manim import *
+
 text = VGroup()
+
 text1 = Tex(r"$\Sigma$", color=RED,font_size=174)
+
 text2 = Text("Math", font="Tex Gyre Chorus", slant=ITALIC, font_size = 58)
+
 text3 = Text("Exercicises", font="Tex Gyre Chorus", slant=ITALIC, font_size = 58)
 
 text.add(text2, text3)
+
 text.arrange(DOWN, center = False, aligned_edge = LEFT)
 
 text.next_to(text1, RIGHT)
@@ -14,10 +19,13 @@ texto_logo = VGroup()
 texto_logo.add(text1, text)
 
 circle_logo = Circle(fill_opacity=5).scale(2.2)
+
 circle_logo.set_fill()
+
 circle_logo.set_color(GREEN)
 
 circle_logo_right_corner = circle_logo.copy().scale(0.3)
+
 texto_logo_right_corner = texto_logo.copy().scale(0.3)
 
 texto_final_1 = Tex(r"Thanks for watching!", color=RED, font_size=100)
@@ -37,7 +45,8 @@ class Logo(Scene):
 
     def logo_right_corner():
         circle_logo_right_corner.to_corner(DOWN + RIGHT)
-        text2 = texto_logo_right_corner.next_to(circle_logo_right_corner, 0 , buff = 1)
+
+text2 = texto_logo_right_corner.next_to(circle_logo_right_corner, 0 , buff = 1)
 
         return VGroup(circle_logo_right_corner, text2)
 
