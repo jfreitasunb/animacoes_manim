fc146363f50e59afc56a8ad1f414cd953ad6c7ee
José Antônio Oliveira de Freitas <jfreitas@unb.br>
2022-08-08 21:28:15 -0300

    Ajustes no texto e na função.

--- logo.py
+++ logo.py
@@ -2,11 +2,8 @@ from manim import *
 
 text_en = VGroup()
 
-text1_br = Text("J", font="Tex Gyre Chorus", slant=ITALIC, font_size = 58)
+text_br = Text("JAF", font="Tex Gyre Chorus", slant=ITALIC, font_size = 58)
 
-text2_br = Text("A", font="Tex Gyre Chorus", slant=ITALIC, font_size = 58)
-
-text3_br = Text("F", font="Tex Gyre Chorus", slant=ITALIC, font_size = 58)
 
 text1_en = Tex(r"$\Sigma$", color=RED,font_size=174)
 
@@ -23,10 +20,6 @@ texto_logo_en = VGroup()
 
 texto_logo_en.add(text1_en, text_en)
 
-texto_logo_br = VGroup()
-
-texto_logo_br.add(text1_br, text2_br, text3_br)
-
 circle_logo = Circle(fill_opacity=5).scale(2.2)
 
 circle_logo.set_fill()
@@ -37,7 +30,7 @@ circle_logo_right_corner = circle_logo.copy().scale(0.1)
 
 texto_logo_en_right_corner = texto_logo_en.copy().scale(0.1)
 
-texto_logo_br_right_corner = texto_logo_en.copy().scale(0.1)
+texto_logo_br_right_corner = text_br.copy().scale(0.1)
 
 texto_final_1_en = Tex("thanks for watching!", color=RED, font_size=100)
 
@@ -58,7 +51,7 @@ class Logo(Scene):
         if (idioma == "en"):
             texto = texto_logo_en.next_to(circle_logo, 0, buff=1).scale(0.9)
         else:
-            texto = texto_logo_br.next_to(circle_logo, 0, buff=1).scale(0.9)
+            texto = text_br.next_to(circle_logo, 0, buff=1).scale(0.9)
 
         self.play(DrawBorderThenFill(circle_logo), Write(texto, **{"run_time" : 6}))
 
@@ -68,15 +61,15 @@ class Logo(Scene):
 
         self.wait(2)
 
-    def logo_right_corner(idioma = "en"):
+    def logo_right_corner(self, texto_br):
         circle_logo_right_corner.to_corner(DOWN + RIGHT)
 
-        if (idioma == "en"):
-            text2 = texto_logo_en_right_corner.next_to(circle_logo_right_corner, 0 , buff = 1)
-        else:
+        if (texto_br == True) :
             text2 = texto_logo_br_right_corner.next_to(circle_logo_right_corner, 0 , buff = 1)
+        else:
+            text2 = texto_logo_en_right_corner.next_to(circle_logo_right_corner, 0 , buff = 1)
 
-        return VGroup(circle_logo_right_corner, text2)
+        self.add(circle_logo_right_corner, text2)
 
     def fechamento(self, idioma = "en"):
 
