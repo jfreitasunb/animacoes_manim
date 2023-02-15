1d919d3e42d72ca40d10703717ea73595ebe2973
José Antônio Oliveira de Freitas <65927652+jfreitasunb@users.noreply.github.com>
2023-02-15 16:26:50 -0300

    Mudando a fonte e o tamalho.

--- logo.py
+++ logo.py
@@ -2,7 +2,7 @@ from manim import *
 
 text_en = VGroup()
 
-text_br = Text("JAF", font="Tex Gyre Chorus", slant=ITALIC, font_size = 158)
+text_br = Text("JAF", font="Beyond Wonderland", slant=ITALIC, font_size = 138)
 
 text1_en = Tex(r"$\Sigma$", color=RED,font_size=174)
 
